efcd0b8e79f4d78fb0acc669af08256dced32dca
Michael Harris <mike.n.harris1@gmail.com>
2023-02-28 15:52:40 -0500

    getting closer

---
+++ src/components/Article.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ArticleList from "./ArticleList";
+
+function Article ({title}) {
+    return (
+        <article>
+            <h3>{title}</h3>
+        </article>
+    )
+}
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ArticleList () {
+    return (
+        <div>
+            <main>
+            <Article title = {posts.title} />
+            </main>
+        </div>
+    )
+}
+export default ArticleList;
